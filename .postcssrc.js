bcd07cb323f3b1f7afc667a81224483beb1b6cc8
SeungHun6450 <toffg6450@naver.com>
2021-06-09 21:49:48 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+// ESM 지원 X
+
+// import autoprefixer from 'autoprefixer'
+
+// export = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+
+// CommonJS를 지원
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
